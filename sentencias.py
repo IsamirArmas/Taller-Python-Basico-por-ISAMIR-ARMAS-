b2d0ed891e8125f4b932303a5ec679a5827ac08a
IsamirArmas <87822535+IsamirArmas@users.noreply.github.com>
2026-03-26 20:42:07 -0600

    Clase 1

---
+++ sentencias.py
@@ -0,0 +1,20 @@
+a = 30
+b = 10
+
+#IF  
+#Indentacion
+# condicion1 Y condicion 2
+if (a>b or a == 10) :
+    print("El numero", a," es mayor a ", b)
+elif a==b :
+    print("el Numero", a, "Es igual a ", b)
+elif a < b :
+    print("Los numeros son diferentes")
+else:
+    print("No se cumple la condicion")
+
+
+#Funcion Input
+nombre = input("Ingresa tu nombre")
+print("Hola mucho gusto", nombre)
+
---
+++ variables.py
@@ -0,0 +1,43 @@
+#Variable
+edad = 27   # Entero 
+estatura = 1.65 #Flotante
+booleana = True  #booleano
+nombre = "Isamir"  #Cadena de texto
+
+#Variables
+a = float(input("Ingresa el primer numero: "))
+b = float(input("Ingresa el segundo numero: "))
+#Operaciones basicas
+suma = a+b  
+resta = a-b
+multiplicacion = a*b
+divicion = a/b
+divicion_entera = a//b
+modulo = a%b
+elevado = a**b
+#PRINT
+print(suma)
+print(resta)
+print(multiplicacion)
+print(divicion)
+print(divicion_entera)
+print(modulo)
+print(elevado)
+#Operaciones comparativas
+print(a == b)   #False
+print(a != b)   #True
+print(a > b)    #True
+print(a < b)    #False
+print(a >= b)    #True
+print(a <= b)  #False
+
+#Manejo de tipos de datos
+numero1 = 1    #entero
+numero2 = "2"  #Cadena de texto
+print(numero1 + int(numero2))   #Suma de enteros: 3
+print(str(numero1) + numero2)   #Concatenando 2 cadenas de texto : 12
+
+cadena1 = "Hola "
+cadena2 = "Mundo"
+print(cadena1 + cadena2)
+
